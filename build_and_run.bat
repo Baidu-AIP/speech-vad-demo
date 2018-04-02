9484e78c7f6b46d2bd27f10387dbcf5d993bc27a
船歌 <yokka47@qq.com>
2018-04-02 19:03:48 +0800

    运行前加个输出

--- build_and_run.bat
+++ build_and_run.bat
@@ -4,9 +4,10 @@ del build\*.* /Q /F
 del output_pcm\*.* /Q /F
 cd build
 
-cmake .. -G "MinGW Makefiles"		&& ^
-make -j4							&& ^
-copy vad-demo.exe ..				&& ^
+cmake .. -G "MinGW Makefiles"					&& ^
+make -j4										&& ^
+copy vad-demo.exe ..							&& ^
+echo #-----------build succeeded----------#		&& ^
 ..\vad-demo.exe
 
 cd ..
